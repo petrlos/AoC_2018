5d080fb6537efd8582ca47dd35eea03f85984cfc
Petr Los <petrlos@gmail.com>
2022-10-30 22:08:21 +0100

    Day 17: Test data working, data not, no star

--- 17/17.py
+++ 17/17.py
@@ -1,6 +1,6 @@
 #Advent of Code 2018: Day 17
 import re
-from collections import defaultdict, Counter
+from collections import defaultdict, Counter, deque
 
 def createField(lines):
     def extendRangeList(interval):
@@ -30,7 +30,7 @@ def drawField(field, maxDepth):
     print("  45678901234567")
     for y in range(0, maxDepth+2):
         print(str(y)[-1], end=" ")
-        for x in range(494, 508):
+        for x in range(1, 1000):
             print(field[(x,y)], end="")
         print(" ")
 
@@ -38,23 +38,46 @@ def fillUpTheField(field, maxDepth):
     activePoints = {(500, 0)}
     while activePoints:
         for point in list(activePoints):
+            dropFound = False
             x,y = point
-            if field[(x,y+1)] == ".": #bellow active point no wall - drop
-                activePoints.discard(point)
+            if field[(x,y+1)] == ".": #bellow active point free space - drop
                 if y+1 <= maxDepth:
                     field[(x,y+1)] = "~"
                     activePoints.add((x,y+1))
-
-        drawField(field, maxDepth)
+                activePoints.discard(point)
+            else:
+                offset = 1
+                while field[(x-offset, y)] != "#": #look left and find a wall or a drop
+                    field[(x-offset, y)] = "~"
+                    if field[(x-offset), y+1] == ".":
+                        activePoints.discard(point)
+                        activePoints.add((x-offset, y))
+                        dropFound = True
+                        break
+                    offset += 1
+                offset = 1
+                while field[(x+offset, y)] != "#": #look right and find a wall or a drop
+                    field[(x+offset, y)] = "~"
+                    if field[(x+offset), y+1] == ".":
+                        activePoints.discard(point)
+                        activePoints.add((x+offset, y))
+                        dropFound = True
+                        break
+                    offset += 1
+                if not dropFound:
+                    activePoints.discard(point)
+                    activePoints.add((x, y-1))
     return Counter(field.values())["~"]
 
 #MAIN
 
-with open("test2.txt") as file:
+#TODO: Test data working, data.txt not
+
+with open("test.txt") as file:
     lines = file.read().splitlines()
 
 maxDepth, field = createField(lines)
-#drawField(field, maxDepth)
-
 result = fillUpTheField(field, maxDepth)
+drawField(field, maxDepth)
+
 print("Total waterdrops filled:", result)
