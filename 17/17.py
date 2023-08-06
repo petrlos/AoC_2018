8d3574de57ab416062e5a20ea33fd6806e347631
Petr Los <petrlos@gmail.com>
2023-08-06 10:20:38 +0200

    Day 17: Generate field

--- 17/17.py
+++ 17/17.py
@@ -2,82 +2,31 @@
 import re
 from collections import defaultdict, Counter, deque
 
-def createField(lines):
-    def extendRangeList(interval):
-        if len(interval) > 1:
-            interval = list(range(interval[0], interval[1] + 1))
-        return interval
-
-    depths = []
-    regNum = re.compile(r"\d+")
+def create_field(lines):
     field = defaultdict(lambda: ".")
-    field[(500,0)] = "+"
     for line in lines:
-        first, second = line.split(", ")
-        x = extendRangeList(list(map(int,regNum.findall(first))))
-        y = extendRangeList(list(map(int,regNum.findall(second))))
-        if line[0] == "y": #switch horizontal/vertical lines
-            x,y = y,x
-        depths += y
-        for xCoord in x:
-            for yCoord in y:
-                field[(xCoord,yCoord)] = "#"
-    return max(depths), field
+        constant, start, end = list(map(int, (re.findall(r"\d+", line))))
+        list_constant = [constant] * (end-start+1)
+        list_range = list(range(start, end+1))
+        if line[0] == "x":
+            coords = (zip(list_constant, list_range))
+        else:
+            coords = (zip(list_range, list_constant))
+        for coord in coords:
+            field[coord] = "#"
+    return field
 
-def drawField(field, maxDepth):
-    print("  44444455555555")
-    print("  99999900000000")
-    print("  45678901234567")
-    for y in range(0, maxDepth+2):
+def draw_field(field):
+    print("  44444455555555 \n  99999900000000 \n  45678901234567")
+    for y in range(0, 14):
         print(str(y)[-1], end=" ")
-        for x in range(1, 1000):
-            print(field[(x,y)], end="")
+        for x in range(494, 508):
+                print(field[(x,y)], end="")
         print(" ")
 
-def fillUpTheField(field, maxDepth):
-    activePoints = {(500, 0)}
-    while activePoints:
-        for point in list(activePoints):
-            dropFound = False
-            x,y = point
-            if field[(x,y+1)] == ".": #bellow active point free space - drop
-                if y+1 <= maxDepth:
-                    field[(x,y+1)] = "~"
-                    activePoints.add((x,y+1))
-                activePoints.discard(point)
-            else:
-                offset = 1
-                while field[(x-offset, y)] != "#": #look left and find a wall or a drop
-                    field[(x-offset, y)] = "~"
-                    if field[(x-offset), y+1] == ".":
-                        activePoints.discard(point)
-                        activePoints.add((x-offset, y))
-                        dropFound = True
-                        break
-                    offset += 1
-                offset = 1
-                while field[(x+offset, y)] != "#": #look right and find a wall or a drop
-                    field[(x+offset, y)] = "~"
-                    if field[(x+offset), y+1] == ".":
-                        activePoints.discard(point)
-                        activePoints.add((x+offset, y))
-                        dropFound = True
-                        break
-                    offset += 1
-                if not dropFound:
-                    activePoints.discard(point)
-                    activePoints.add((x, y-1))
-    return Counter(field.values())["~"]
-
-#MAIN
-
-#TODO: Test data working, data.txt not
-
+#MAINd
 with open("test.txt") as file:
     lines = file.read().splitlines()
 
-maxDepth, field = createField(lines)
-result = fillUpTheField(field, maxDepth)
-drawField(field, maxDepth)
-
-print("Total waterdrops filled:", result)
+field = create_field(lines)
+draw_field(field)
