227bc746b0ab718ca86864b4bbdaaede702306b0
Petr Los <petrlos@gmail.com>
2024-04-10 21:47:08 +0200

    Day 17: both stars

--- 17/17.py
+++ 17/17.py
@@ -1,7 +1,6 @@
 #Advent of Code 2018: Day 17
 import re
-from collections import deque, namedtuple, defaultdict
-from icecream import ic
+from collections import namedtuple, defaultdict
 
 def draw_underground(underground, min_row, max_row, min_col, max_col):
     for r in range(min_row-2, max_row+3):
@@ -31,20 +30,36 @@ def generate_underground(lines):
     max_col = max(underground.keys(), key=lambda item: item[1])[1]
     return underground, min_row, max_row, min_col, max_col
 
+def drop_down(drop):
+    to_be_filled = set()
+    new_drops = []
+    r,c = drop.coord
+    while underground[(r + 1, c)] in "." and not r > max_row:
+        r += 1
+        to_be_filled.add((r,c))
+        if underground[(r + 1, c)] in "~#":
+            new_drops.append(Drop((r, c), "s"))
+            return new_drops, to_be_filled # "bottom" to be filled - either hard, or water
+    return [], to_be_filled #  toplayer of filled reservoir
+
 def spread_to_sides(drop):
+    to_be_filled = {drop.coord}
     new_drops = []
     for i in [-1, +1]: #-1 = spread left, +1 = spread right
         row, col = drop.coord
-        while underground[(row,col + i)] != "#":
-            underground[(row,col + i)] = "~"
+        while underground[(row,col + i)] != "#": #look left + right as long as border found
+            to_be_filled.add((row, col+i))
             if underground[(row+1, col+i)] == ".":
                 new_drops.append(Drop((row, col+i), "d"))
                 break
             col += i
     if len(new_drops) == 0:
+        water_type = "~" #in reservoir - will be filled with layer above
         row, col = drop.coord
         new_drops.append(Drop((row-1,col), "s"))
-    return new_drops
+    else:
+        water_type = "|" #top layer
+    return new_drops, water_type, to_be_filled
 
 #MAIN
 with open("data.txt") as file:
@@ -53,31 +68,25 @@ with open("data.txt") as file:
 underground, min_row, max_row, min_col, max_col = generate_underground(lines)
 
 Drop = namedtuple("Drop", ["coord", "dir"]) #direction: d=down, s=left+right
-queue = deque([Drop((0,500), "d")])
+queue = [(Drop((0,500), "d"))]
 
 while queue:
-    current = queue.popleft()
-    r,c = current.coord
-    if r > max_row:
-        continue
+    current = queue.pop()
     if current.dir == "d": #drop down
-        if underground[(r+1,c)] == ".": #space beneath - drop
-            queue.append(Drop((r+1,c), "d"))
-        if underground[(r+1,c)] == "#": #wall - spread left + righ
-            queue.append(Drop((r,c), "s"))
-    if current.dir == "s":
-        new_drops = spread_to_sides(current)
-        for new_drop in new_drops:
-            queue.append(new_drop)
-    underground[current.coord] = "~"
-    ic(queue)
-draw_underground(underground, min_row, max_row, min_col, max_col)
+        new_drops, to_be_filled = drop_down(current)
+        water_type = "|"
+    else: #else is only "s" - spread to sides
+        new_drops, water_type, to_be_filled = spread_to_sides(current)
+    underground.update({filled: water_type for filled in to_be_filled})
+    queue += new_drops
 
-counter = 0
+part1 = part2 = 0
 for coord, value in underground.items():
-    if coord[0] >= min_row and value == "~":
-        counter += 1
+    if max_row >= coord[0] >= min_row:
+        if value in "|~": # each drop
+            part1 += 1
+        if value == "~": # drops in reservoirs only
+            part2 += 1
 
-print(counter)
-
-#TODO: Not working, minor issues - see test2.txt
+print("Part 1:", part1)
+print("Part 2:", part2)
