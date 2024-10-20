d035b08ea89f50f63575c31ecd80254b2c540c51
Petr Los <petrlos@gmail.com>
2024-10-20 11:42:30 +0200

    Day 25: working

--- 25/25.py
+++ 25/25.py
@@ -1,36 +1,50 @@
 #Advent of Code 2018: Day 25
-import re
+from datetime import datetime
+timer_start = datetime.now()
 
-def parseData(lines):
-    regNum = re.compile(r"-?\d+")
-    points = []
-    for line in lines:
-        #converts list of strings to list of ints
-        points.append(list(map(int,regNum.findall(line))))
-    return points
+def check_distance(coord, constellations):
+    #check for already checked coords in which constellations is MD <= 3
+    where = list()
+    for id, constellation in enumerate(constellations):
+        for coord_checked in constellation:
+            if manh_distance(coord_checked, coord) <= 3:
+                where.append(id)
+    return where
 
-def manhDistance(a, b):
+def merge_constellations(constellations, where, coord):
+    new_constellations = []
+    merged = set()
+    merged.add(tuple(coord))
+    for wh in where:
+        merged = merged | (constellations[wh])
+    for id, constellation in enumerate(constellations):
+        if id not in where:
+            new_constellations.append(set(constellation))
+    new_constellations.append(merged)
+    return new_constellations
+
+def manh_distance(a, b): #universal function for manhattan distance, works in n-D space
     return sum(abs(val1-val2) for val1, val2 in zip(a,b))
 
 #MAIN
-with open("test.txt") as file:
+
+with open("data.txt") as file:
     lines = file.read().splitlines()
 
-points = parseData(lines)
+coords = set(tuple(map(int, line.split(","))) for line in lines)
 
-##TODO: not working - some points may join smaller groups into larger group -> total count is too high
+constellations = []
 
-groups = [ [points[0]] ]
-for point in points:
-    pointGroupFound = False
-    for group in groups[:]:
-        for groupPoint in group[:]:
-            if manhDistance(point, groupPoint) <=3:
-                group.append(point)
-                pointGroupFound = True
-    if not pointGroupFound:
-        groups.append([point])
+for coord in coords:
+    where = check_distance(coord, constellations)
+    if len(where) == 0: #no match found - create new constelation
+        new_const = set()
+        new_const.add(tuple(coord))
+        constellations.append(new_const)
+    elif len(where) == 1: #add to existing
+        constellations[where[0]].add(coord)
+    else: #merge
+        constellations = merge_constellations(constellations, where, coord)
 
-print(len(groups))
-for group in groups:
-    print(group)
+print("Part 1:", len(constellations))
+print("Runtime:", datetime.now() - timer_start)
