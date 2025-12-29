35855f532f022cfec52170ce962d2b5dfbfe256a
petrlos <petrlos@gmail.com>
2025-12-29 23:07:32 +0100

    Day 24: Part 1

--- 24/24.py
+++ 24/24.py
@@ -1,9 +1,11 @@
 #Advent of Code 2018: Day 24
 import re
 from icecream import ic
+import time
 
 class Unit:
-    def __init__(self, which_army, line):
+    def __init__(self, which_army, line, group_id):
+        self.group_id = group_id
         self.which_army = which_army
         self.count = int(re.search(r"(\d+) units", line).group(1))
         self.hp = int(re.search(r"(\d+) hit", line).group(1))
@@ -30,44 +32,80 @@ class Unit:
         return self.count * max(self.dmg_given)
 
     def __repr__(self):
-        which = dict({0: "Immune:", 1: "Infect:"})
-        return f"Army {which[self.which_army]} Units:{self.count}, Dmg.:{max(self.dmg_given)}, HP:{self.hp}, Init.:{self.init}, Weak.:{self.weaknesses}, Dmg:{self.dmg_given}"
+        return f"Army {which[self.which_army]} {self.group_id}: Units: {self.count}"
+
+def all_dead(armies):
+    immune = []
+    infection = []
+    for army in armies:
+        if army.which_army == 0: immune.append(army.count)
+        elif army.which_army == 1: infection.append(army.count)
+    if sum(immune) == 0: return False
+    if sum(infection) == 0: return False
+    return True
 
 #MAIN
-with open("test.txt") as file:
+with open("data.txt") as file:
     lines = file.read().split("\n")
 
 dmg_type = dict(zip("fire,cold,slashing,radiation,bludgeoning".split(","), (range(5))))
+which = dict({0: "Immune", 1: "Infect"})
+
 armies = []
 which_army = 0
+group_id = 1
 for line in lines[1:]:
     if line == "": continue
     if "Infection" in line:
         which_army += 1
+        group_id = 1
         continue
-    unit = Unit(which_army, line)
+    unit = Unit(which_army, line, group_id)
     armies.append(unit)
+    group_id += 1
 
-#sort via decreasing efficient power, by tie by initiative
-armies.sort(key=lambda g: (-g.eff_power, -g.init))
-for attacker in armies:
-    attacker.target = None
-    attacker.theor_dmg = -1
-    for target in armies:
-        if target.selected: continue
-        if attacker.which_army == target.which_army: continue
-        theor_dmg = max([x*y*attacker.count for x, y in zip(attacker.dmg_given, target.weaknesses)])
-        if theor_dmg == 0: continue
-        if attacker.target is None:
-            attacker.target = target
-            attacker.theor_dmg = theor_dmg
-        elif ((theor_dmg, target.eff_power, target.init) >
-                (attacker.theor_dmg, attacker.target.eff_power,attacker.target.init)):
-            attacker.target = target
-            attacker.theor_dmg = theor_dmg
-    if attacker.target is not None: attacker.target.selected = True
+while all_dead(armies):
+    #sort via decreasing efficient power, by tie by initiative
+    armies.sort(key=lambda g: (-g.eff_power, -g.init))
+    #find target
+    for attacker in armies:
+        if attacker.count == 0: continue
+        attacker.target = None #initialize
+        attacker.theor_dmg = -1
+        for target in armies:
+            if target.selected: continue
+            if attacker.which_army == target.which_army: continue
+            if target.count == 0: continue
+            theor_dmg = max([x*y*attacker.count for x, y in zip(attacker.dmg_given, target.weaknesses)])
+            if attacker.target is None: #did not find any target
+                attacker.target = target
+                attacker.theor_dmg = theor_dmg
+            #target found - compare by dmg, by effective power, by initiative
+            elif ((theor_dmg, target.eff_power, target.init) >
+                    (attacker.theor_dmg, attacker.target.eff_power,attacker.target.init)):
+                attacker.target = target
+                attacker.theor_dmg = theor_dmg
+        if attacker.target is not None: attacker.target.selected = True
 
-for attacker in armies:
-    ic(attacker)
-    ic(attacker.target)
-    ic(attacker.theor_dmg)
+    #sort by initiative
+    armies.sort(key=lambda g: (-g.init))
+    #attack
+    for attacker in armies:
+        if attacker.count == 0: continue #all dead
+        if attacker.target is None: continue
+        dmg_done = max([x*y*attacker.count for x, y in zip(attacker.dmg_given, attacker.target.weaknesses)])
+        kills = min(attacker.target.count, dmg_done // attacker.target.hp)
+        #print(f"{which[attacker.which_army]} {attacker.group_id}: deals {dmg_done} to group {attacker.target.group_id} and kills {kills} units.")
+        attacker.target.count -= kills
+
+    for army in armies:
+        army.target = None
+        army.selected = False
+
+immune = []
+infection = []
+for army in armies:
+    if army.which_army == 0: immune.append(army.count)
+    elif army.which_army == 1: infection.append(army.count)
+print("Immune:", sum(immune))
+print("Infection:", sum(infection))
